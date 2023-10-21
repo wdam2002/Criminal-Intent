9c5a0a3a73acc705b56e8089e85b6bc615e7d401
wdam2002 <wdam@cpp.edu>
2023-10-20 21:13:59 -0700

    Initial commit

---
+++ app/src/main/java/database/CrimeDao.kt
@@ -0,0 +1,28 @@
+package database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.bignerdranch.android.criminalintent.Crime
+import kotlinx.coroutines.flow.Flow
+import java.util.UUID
+
+@Dao
+interface CrimeDao {
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): Flow<List<Crime>>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    suspend fun getCrime(id: UUID): Crime
+
+    @Update
+    suspend fun updateCrime(crime: Crime)
+
+    @Insert
+    suspend fun addCrime(crime: Crime)
+
+    @Delete
+    suspend fun deleteCrime(crime: Crime)
+}
